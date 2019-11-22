02dcd40f6693a3e8b9456783b9a6fb2849fa07a4
Howard Kennedy <hkennedyvi@gmail.com>
2019-11-22 12:59:10 -0800

    hidden number values added for time slots, javascript updated with color coordination function (needs work), form added to html

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-var thisHour = moment().format('H');
-
+var thisHour = parseInt(moment().format('H'));
+var slotHour = parseInt($("#hideme").text());
 console.log(thisHour);
+console.log(slotHour);
 
 function update() {
-    $('#currentDay').html(moment().format('MMMM Do YYYY H:mm:ss'));
+    $('#currentDay').html(moment().format('LLLL'));
 }
 
 setInterval(update, 1000);
 update();
+
+$(".hour").each(function () {
+    //var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    
+    for (i=0; i<slotHour.length; i++)
+
+    if (slotHour < thisHour) {
+        $(this).addClass("past");
+    }
+    else if (slotHour === thisHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+    else $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+    
+});
